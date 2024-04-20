c46a2aad75ac96611367f5b68adeb86f664782b0
shi-yingyingjiang <136897416+shi-yingyingjiang@users.noreply.github.com>
2024-04-20 13:18:35 +0800

    Add files via upload

---
+++ nonebot_plugin_zsmeme/__init__.py
@@ -0,0 +1,28 @@
+from nonebot import on_command
+from nonebot.adapters.onebot.v11 import MessageSegment
+from httpx import AsyncClient
+from nonebot.plugin import PluginMetadata
+from .config import the_meme_url
+from io import BytesIO
+
+
+__plugin_meta__ = PluginMetadata(
+    name='战双表情',
+    description='发送bwiki的战双表情',
+    usage='战双表情/zsmeme',
+    extra={}
+)
+
+
+pgr_meme = on_command("战双表情", aliases={'zsmeme','pgrmeme'})
+
+
+@pgr_meme.handle()
+async def get_img():
+    async with AsyncClient() as client:
+        image = await client.get(the_meme_url())
+        picbytes = BytesIO(image.content).getvalue()
+    try:
+        await pgr_meme.send(MessageSegment.image(picbytes))
+    except:
+        await pgr_meme.send(f'发送失败')
